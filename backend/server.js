f1bccc59fcc5dbd52191bd82e2ec71836c4b860d
nicolasrodpra <nicolaarodriguez231206@gmail.com>
2026-03-12 12:04:55 -0500

    Home

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebaseKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 const app = express();
 
@@ -12,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("API SafeHome funcionando");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
